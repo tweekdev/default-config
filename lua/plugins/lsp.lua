7bed8c7b24da698c43743abb75df45445394dde2
tweekdev <tja.theau54@gmail.com>
2025-04-09 14:03:30 +0200

    update config neo tree

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,15 +15,38 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		dependencies = { "jose-elias-alvarez/typescript.nvim" },
 		lazy = false,
 		config = function()
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities.textDocument.completion =
 				require("cmp_nvim_lsp").default_capabilities().textDocument.completion
 
+			local function organize_imports()
+				local params = {
+					command = "_typescript.organizeImports",
+					arguments = { vim.api.nvim_buf_get_name(0) },
+					title = "",
+				}
+				vim.lsp.buf.execute_command(params)
+			end
+
 			local lspconfig = require("lspconfig")
 			lspconfig.ts_ls.setup({
+				on_attach = function(client, bufnr)
+					client.server_capabilities.documentFormattingProvider = false
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						command = "OrganizeImports",
+					})
+				end,
 				capabilities = capabilities,
+				commands = {
+					OrganizeImports = {
+						organize_imports,
+						description = "Organize Imports",
+					},
+				},
 			})
 			lspconfig.solargraph.setup({
 				capabilities = capabilities,
@@ -44,7 +67,7 @@ return {
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			-- vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gd", "<cmd>Telescope lsp_definition<CR>", opts) -- show lsp definitions
-			vim.keymap.set("n", "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", opts)
+			vim.keymap.set("n", "<leader>co", "<cmd>OrganizeImports<CR>", opts)
 			vim.keymap.set("n", "<leader>gr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 			-- vim.keymap.set("n", "<leader>gR", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
